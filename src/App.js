aa6f45046c4f6229ba0fd5e6178472d87e28307c
karinagb <karin.gb@gmail.com>
2023-03-08 15:29:03 -0600

    Adding style & react gh pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import { useState } from "react";
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    setCalc(calc + value);
+  }
+
+  const createDigits = () => {
+
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button 
+          onClick={() => updateCalc(i.toString())} 
+          key={i}>
+          {i}
+          </button>
+      )
+    }
+
+    return digits;
+
+
+  }
+
+
 	return (
 		<div className="App">
 			<div className="calculator">
 				<div className="display">
-					<span>(0)</span> 0
+					{ result? <span>(0)</span> : ''}
+          {calc || "0"}
 				</div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
 
           <button>DEL</button>
         </div>
         <div className="digits">
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          {createDigits()}
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+          <button onClick={() => updateCalc('=')}>=</button>
         </div>
 
 			</div>
